5ccc5bac7f89d00f4f2676059ea84dd7165ac018
Douglas Anderson <douglas.anderson@outlook.com>
2024-02-08 20:18:45 +0200

    First commit

---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	export let data;
+</script>
+
+<h1>{data.title}</h1>
+<div>{@html data.content}</div>
+
+foo bar
---
+++ svelte.config.js
@@ -0,0 +1,20 @@
+import adapter from '@sveltejs/adapter-static';
+
+const config = {
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html',
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
+};
+
+export default config;
