6e2e1914365cb23c0a6fb2b68c171ae17cd8fa4d
Douglas Anderson <douglas.anderson@outlook.com>
2024-02-09 09:32:46 +0200

    Misc

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import preprocess from 'svelte-preprocess';
 
 const config = {
 	kit: {
@@ -13,7 +13,9 @@ const config = {
 			strict: true
 		})
 	},
-	preprocess: vitePreprocess()
+	preprocess: preprocess({
+    postcss: true
+  })
 };
 
 export default config;
