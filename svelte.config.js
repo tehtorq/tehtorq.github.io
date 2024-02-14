88a4d26290f85796d36dc2da42f57b9fb051c491
Douglas Anderson <douglas.anderson@outlook.com>
2024-02-14 08:46:25 +0200

    Misc

--- svelte.config.js
+++ svelte.config.js
@@ -3,15 +3,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html',
-			precompress: false,
-			strict: true
-		}),
+		adapter: adapter(),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
